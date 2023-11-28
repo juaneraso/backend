c497147c72aa6337bfcb3ea4ba2ea57c57f8ef05
Juan <juangui9508@hotmail.com>
2023-11-27 20:53:06 -0500

    promises-two

--- Promises/index.js
+++ Promises/index.js
@@ -1,5 +1,33 @@
 const axios = require("axios");
 
-axios.get("https://rickandmorty.com/api/character").then((res) => {
-  console.log(res.data);
+// axios.get("https://rickandmortyapi.com/api/episode").then(
+//   (res) => console.log(res.data),
+//   (error) => console.log(error)
+// );
+
+// fetch("https://rickandmortyapi.com/api/episode")
+//   .then((response) => response.json()) // Return implicito
+//   .then((data) => console.log(data));
+
+const miPromesa = new Promise((resolve, reject) => {
+  resolve("Todo mal");
+  // resolve("Holis");
 });
+
+miPromesa
+  .then(
+    (response) => {
+      return "todo bien";
+    }, // Succes handler
+    (error) => console.log("Rechazada")
+  )
+  .then(
+    (response) => {
+      return response;
+    },
+    (error) => console.log(error)
+  )
+  .then(
+    (respuesta) => console.log(respuesta),
+    (error) => console.log(error)
+  );
