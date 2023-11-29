7ed982e3a5bd50ee36c8863bbdf9dd117a5b4aa7
Juan <juangui9508@hotmail.com>
2023-11-29 13:13:53 -0500

    middlewares

--- Express/src/app.js
+++ Express/src/app.js
@@ -7,14 +7,17 @@ const server = express();
 // La request pasa por el middleware que creamos , luego por morgan y luego por el enrutador
 
 server.use((req, res, next) => {
+  // Primer middleware
   // middleware
   console.log("Estamos pasando por un middleware");
   console.log(req.url);
   next();
 });
 
-server.use(morgan("dev"));
+server.use(morgan("dev")); //Segundo middleware
 
-server.use(mainRouter);
+server.use(express.json()); // Middleware que convierte los objetos json en objetos de javascript
+
+server.use(mainRouter); //Tercer middleware
 
 module.exports = server;
--- Express/src/routes/apiRouter.js
+++ Express/src/routes/apiRouter.js
@@ -1,21 +1,56 @@
 const { Router } = require("express");
 
+const database = require("../database");
+
 const apiRouter = Router();
 
 apiRouter.get("/", (req, res) => {
-  res.status(200).send("Estamos en API");
+  const { name } = req.query;
+  if (name) {
+    const searchResults = database.filter((char) => char.name === name);
+    res.status(200).json(searchResults);
+  } else {
+    res.status(200).json(database);
+  }
 });
 
-apiRouter.get("/characters", (req, res) => {
-  res.status(200).send("Estamos en API");
+apiRouter.get("/:id", (req, res) => {
+  // const id = req.params.id;
+  try {
+    const { id } = req.params;
+    const character = database.find((char) => char.id == id);
+    if (!character) {
+      throw Error("Character does not exist");
+    }
+
+    res.status(200).json(character);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
 });
 
-apiRouter.get("/location", (req, res) => {
-  res.status(200).send("Estamos en API");
+apiRouter.post("/", (req, res) => {
+  console.log(req.body);
+  const { name, gender } = req.body;
+
+  const newCharacter = {
+    id: database.length + 1,
+    name,
+    gender,
+    species: "Human",
+    status: "Alive",
+  };
+  database.push(newCharacter);
+
+  res.status(200).json({ Created: "Ok" });
 });
 
-apiRouter.get("/episodes", (req, res) => {
-  res.status(200).send("Estamos en API");
-});
+// apiRouter.get("/location", (req, res) => {
+//   res.status(200).send("Estamos en API");
+// });
+
+// apiRouter.get("/episodes", (req, res) => {
+//   res.status(200).send("Estamos en API");
+// });
 
 module.exports = apiRouter;
