dec7d3768d3113ab39133e934ded1c75d08ddac3
Juan <juangui9508@hotmail.com>
2023-12-22 09:40:03 -0500

    changes server

--- Sequelize/src/server.js
+++ Sequelize/src/server.js
@@ -1,11 +1,5 @@
 const express = require("express");
 const morgan = require("morgan");
-const findAllEpisodes = require("./controllers/findAllEpisodes");
-const createEpisode = require("./controllers/createEpisode");
-const createCharacter = require("./controllers/createCharacter");
-const findCharacterById = require("./controllers/findCharacterById");
-const deleteCharacter = require("./controllers/deleteCharacter");
-const createdBulkEpisodes = require("./controllers/createBulkEpisodes");
 
 const mainRouter = require("./routes/index");
 
@@ -15,11 +9,5 @@ server.use(express.json());
 server.use(morgan("dev")); // middlware morgan
 
 server.use(mainRouter);
-
-// server.post("/episode/bulk", async (req, res) => {
-//   const { episodes } = req.body;
-//   const created = await createdBulkEpisodes(episodes);
-//   res.status(200).json({ response: "created" });
-// });
 
 module.exports = server;
